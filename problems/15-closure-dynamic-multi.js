0cb7bbcbed325cd16f9006b2170a051fc7454eda
helcio <h.andre@giap.pl>
2023-11-15 23:17:12 +0100

    Fix problem 16 15-closure-dynamic-multi

--- problems/15-closure-dynamic-multi.js
+++ problems/15-closure-dynamic-multi.js
@@ -19,7 +19,9 @@ multiplyByFive(5); // returns 25
 ***********************************************************************/
 
 function dynamicMultiply(num) {
-  // Your code here
+  return function (secondNum) {
+    return secondNum * num;
+  };
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
