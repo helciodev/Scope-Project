7486542c91f43e127e6bc745de2467a91126e867
helcio <h.andre@giap.pl>
2023-11-15 13:39:53 +0100

    Fix problem 12 11-lazy-adder

--- problems/10-count-down.js
+++ problems/10-count-down.js
@@ -27,10 +27,25 @@ Example 4:
   console.log(threeDays()); // prints "Happy New Year!"
 
 ***********************************************************************/
-function countDownTimer(num) {
-  // Your code here
-}
+function countDownTimer(num) {}
 
+// Example 1:
+console.log(countDownTimer(0)); // prints "Happy New Year!"
+
+// Example 3:
+let oneDay = countDownTimer(1); // returns a function
+console.log(oneDay()); // prints "Happy New Year!"
+
+// Example 3:
+let twoDays = countDownTimer(2); // returns a function
+console.log(twoDays()); // returns a function
+console.log(twoDays()); // prints "Happy New Year!"
+
+// Example 4:
+let threeDays = countDownTimer(3); // returns a function
+console.log(threeDays()); // returns a function
+console.log(threeDays()); // returns a function
+console.log(threeDays()); // prints "Happy New Year!"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = countDownTimer;
